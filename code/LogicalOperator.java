1e01db915375930573dff0e6b8642d7651922ac3
luca <luca1230@gmail.com>
2020-09-11 10:19:49 -0700

    installed junit and made functions for legalSimple and legalComplex

--- code/LogicalOperator.java
+++ code/LogicalOperator.java
@@ -1,20 +1,44 @@
 public class LogicalOperator {
 
-    private static String regex = "([~]*[a-z]{1}){1}((\\s)*(=>|<=|<=*>|[|,&]){1}(\\s)*([~]*[a-z]{1}){1})*";
+    // A regular expression to find if a string is a legal logical expression
+    private static final String legalSentenceRegex = "([~]*[a-z]{1}){1}((\\s)*(=>|<=|<=*>|[|,&]){1}(\\s)*([~]*[a-z]{1}){1})*";
+    private static final String legalSimpleRegex = "[a-z]{1}";
+    private static final String negationRegex = "[~]*[a-z]{1}";
+    private static final String splitRegex = "[=*>|<=*|<=*>|[|,&]]";
 
     public static void main(String[] args) {
-        String testCase1 = "a =>b";
-        String testCase2 = "a<=a";
-        String testCase3 = "~b<==>~~~a";
-        String testCase4 = "a<=>b | a";
 
-        System.out.println(legal(testCase1));
-        System.out.println(legal(testCase2));
-        System.out.println(legal(testCase3));
-        System.out.println(legal(testCase4));
     }
 
-    public static boolean legal(String str) {
-        return str.matches(regex);
+    private static boolean legalSimple(String simple) {
+        return simple.matches(legalSimpleRegex);
+    }
+
+    /**
+     * Returns true if the sentence is a legal complex sentence. It will return true if it is a simple
+     * sentence. Some examples of legal complex sentences are ~~a or a => ~~b.
+     * @param complex The sentence to check
+     * @return If the sentence is a legal logical sentence
+     */
+    private static boolean legalComplex(String complex) {
+        if (negation(complex) || legalSimple(complex)) {
+            return true;
+        }
+
+        // We know the sentence is not a negation, therefore it must have a biconditional, implication,
+        // conjunction, or a disjunction
+        String[] splitComplex = complex.split(splitRegex, 1); // We split the string into 2 parts
+
+        return legalComplex(splitComplex[0]) || legalComplex(splitComplex[1]);
+    }
+
+    /**
+     * Finds if a string contains exactly one negation. A negation can be in the form of anything
+     * similar to ~a or ~~~~z.
+     * @param negation The string to check
+     * @return If the string is a negation
+     */
+    public static boolean negation(String negation) {
+        return negation.matches(negationRegex);
     }
 }
