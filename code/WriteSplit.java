d55d8fe2c8c58a439bee608d3cc77decf2658d6a
luca <luca1230@gmail.com>
2020-09-14 13:32:12 -0700

    added helper function for testing

--- code/WriteSplit.java
+++ code/WriteSplit.java
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.Test;
+import java.util.Random;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.function.Function;
 
 public class WriteSplit {
 
@@ -14,7 +14,6 @@ public class WriteSplit {
         return null;
     }
 
-
     public String[] split(String str, Function<String, String[]> backend) {
         return backend.apply(str);
     }
@@ -23,7 +22,19 @@ public class WriteSplit {
     public void testSplit() {
     }
 
+    private String generateRandomString(int length, String alphabet) {
+        Random random = new Random();
+
+        StringBuilder string = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            string.append(alphabet.charAt(random.nextInt(alphabet.length())));
+        }
+
+        return string.toString();
+    }
+
     private String generateRandomString(int length) {
-        return null;
+        return generateRandomString(length, "qwertyuiopasdfghjklzxcvbnm");
     }
 }
