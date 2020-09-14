8ce675d5b4a1679cbf9144aa85cb73da557f215e
luca <luca1230@gmail.com>
2020-09-14 13:37:08 -0700

    added regex as parameter to split and added random test cases

--- code/WriteSplit.java
+++ code/WriteSplit.java
@@ -1,25 +1,33 @@
 import org.junit.jupiter.api.Test;
 import java.util.Random;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WriteSplit {
 
-    public String[] bruteForce(String str) {
+    public String[] bruteForce(String str, String regex) {
         return null;
     }
 
-    public String[] split(String str) {
+    public String[] split(String str, String regex) {
         return null;
     }
 
-    public String[] split(String str, Function<String, String[]> backend) {
-        return backend.apply(str);
+    public String[] split(String str, String regex, BiFunction<String, String, String[]> backend) {
+        return backend.apply(str, regex);
     }
 
     @Test
     public void testSplit() {
+        // TODO: add manual test cases
+
+        for (int i = 0; i < 25; i++) {
+            String randomString = generateRandomString(100);
+            String regex = generateRandomString(1);
+            assertArrayEquals(randomString.split(regex), split(randomString, regex));
+        }
     }
 
     private String generateRandomString(int length, String alphabet) {
