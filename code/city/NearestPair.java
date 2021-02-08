548dba69354b2a754d41eaf106d40f8dde6a5b95
luca <luca1230@gmail.com>
2021-02-08 13:12:06 -0800

    added NearestPair class

---
+++ code/city/NearestPair.java
@@ -0,0 +1,14 @@
+package city;
+
+import java.util.ArrayList;
+
+public class NearestPair {
+
+    public static String findMinDistances(ArrayList<String> cities, double[][] distances) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
