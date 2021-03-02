c6a1fe65c2b288e2426a142835b7eaca06bc9b06
luca <luca1230@gmail.com>
2021-03-02 15:37:50 -0800

    added merge sort

--- code/util/Sorting.java
+++ code/util/Sorting.java
@@ -68,9 +68,45 @@ public class Sorting {
         }
     }
 
+    public static <T extends Comparable<? super T>> void mergeSort(List<T> list) {
+        mergeSort(list, 0, list.size() - 1);
+    }
+
+    private static <T extends Comparable<? super T>> void mergeSort(List<T> list, int s, int e) {
+        if (e - s == 0) {
+            return;
+        } else if (e - s == 1) {
+            if (list.get(s).compareTo(list.get(e)) <= 0) {
+                return;
+            } else {
+                T h = list.get(s);
+                list.set(s, list.get(e));
+                list.set(e, h);
+                return;
+            }
+        }
+        int split = s + (e - s + 1) / 2;
+        mergeSort(list, s, split - 1);
+        mergeSort(list, split, e);
+
+        // merge
+        var copy = list.subList(s, e + 1).toArray();
+        for (int i = s, j = s, k = split; j < split || k < copy.length; i++) {
+            if (j >= split) {
+                list.set(i, (T) copy[k++ - s]);
+            } else if (k - s >= copy.length) {
+                list.set(i, (T) copy[j++ - s]);
+            } else if (((T) copy[j - s]).compareTo((T) copy[k - s]) <= 0) {
+                list.set(i, (T) copy[j++ - s]);
+            } else {
+                list.set(i, (T) copy[k++ - s]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        var list = new ArrayList(List.of(1, 2, 3, 8, 4, 5, 9, 6));
-        threeWaySort(list);
+        var list = new ArrayList(List.of(7, 0, 1, 2, 3, 8, 4, 5, 9, 6));
+        mergeSort(list);
         System.out.println(list.toString());
     }
 }
